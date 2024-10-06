649578d0fdfdce052b6f36574d4f4215ca5931e0
kesojin <kesojincharadze13@gmail.com>
2024-10-06 13:17:35 +0400

    add Task1

---
+++ Week4HW/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+#region Task1
+    Console.WriteLine("Please, input number");
+    int number1 = int.Parse(Console.ReadLine());
+    if (number1 % 5 == 0)
+    {
+        Console.WriteLine("Yes");
+    }
+    else
+    {
+        Console.WriteLine("No");
+    }
+#endregion

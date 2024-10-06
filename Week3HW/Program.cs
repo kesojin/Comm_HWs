a31e5f91fc09234f6bf0054ee45022c2cad8df58
kesojin <kesojincharadze13@gmail.com>
2024-10-06 13:15:21 +0400

    new project

---
+++ Week3HW/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.BackgroundColor = ConsoleColor.Blue;
+Console.WriteLine("Keso Jincharadze");
+Console.WriteLine(Console.ReadLine());

3898ad9d88b3a135730912e71c4443f0b86fa9b5
kesojin <kesojincharadze13@gmail.com>
2024-10-06 13:09:10 +0400

    add line

--- Comm_HWs/Program.cs
+++ Comm_HWs/Program.cs
@@ -1,3 +1,3 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
-Console.WriteLine("Here we'll have all the homeworks organized.");
+Console.WriteLine("We'll have all the homeworks organized here.");

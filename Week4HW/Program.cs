688ad0cb03b69999fe3d6796137c22c09297917c
kesojin <kesojincharadze13@gmail.com>
2024-10-06 13:20:38 +0400

    add Task2

--- Week4HW/Program.cs
+++ Week4HW/Program.cs
@@ -1,14 +1,59 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 #region Task1
-    Console.WriteLine("Please, input number");
-    int number1 = int.Parse(Console.ReadLine());
-    if (number1 % 5 == 0)
-    {
-        Console.WriteLine("Yes");
-    }
-    else
-    {
-        Console.WriteLine("No");
-    }
+Console.WriteLine("Please, input number");
+int number1 = int.Parse(Console.ReadLine());
+if (number1 % 5 == 0)
+{
+    Console.WriteLine("Yes");
+}
+else
+{
+    Console.WriteLine("No");
+}
+#endregion
+
+#region Task2
+Console.WriteLine("Insert the first number");
+int x = int.Parse(Console.ReadLine());
+Console.WriteLine("Insert the second number");
+int y = int.Parse(Console.ReadLine());
+
+int result;
+
+result = x + y;
+Console.WriteLine(result);
+
+result = x * y;
+Console.WriteLine(result);
+
+if (x > y)
+{
+    result = x - y;
+    Console.WriteLine(result);
+}
+else if (y > x)
+{
+    result = y - x;
+    Console.WriteLine(result);
+}
+
+if (x > y)
+{
+    result = x / y;
+    Console.WriteLine(result);
+}
+else if (y == 0)
+{
+    Console.WriteLine("Not Allowed to Devide by Zero");
+}
+else if (y > x)
+{
+    result = y / x;
+    Console.WriteLine(result);
+}
+else if (x == 0)
+{
+    Console.WriteLine("Not Allowed to Devide by Zero");
+}
 #endregion

643e673f3ca0f6b1ccc16219350b8587ee0510d6
kesojin <kesojincharadze13@gmail.com>
2024-10-06 13:22:54 +0400

    add Task5

--- Week4HW/Program.cs
+++ Week4HW/Program.cs
@@ -88,3 +88,15 @@ for (int i = 1; i < 10; i++)
 }
 #endregion
 
+#region Task5
+Console.WriteLine("Please, enter a number");
+int number5 = int.Parse(Console.ReadLine());
+
+for (int i = 1; i < number5; i++)
+{
+    if (i % 2 == 0)
+    {
+        Console.WriteLine(i * i);
+    }
+}
+#endregion
